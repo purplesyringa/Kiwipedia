f61d179eb50c9ca1c406ea710fe9ac708679d967
Ivanq <imachug@yandex.ru>
2018-01-23 21:57:16 +0300

    Add 'Init Hub' page

---
+++ main/src/router_pages/init-hub/init-hub.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<h1>Init Hub</h1>
+		<p>
+			Choose settings for your new hub.
+		</p>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+		name: "init-hub"
+	};
+</script>
--- main/src/router_pages/routes.js
+++ main/src/router_pages/routes.js
@@ -1,5 +1,6 @@
 import Home from "./home/home.vue";
 import NewLanguage from "./new-language.vue";
+import InitHub from "./init-hub/init-hub.vue";
 
 import * as config from "../config.js";
 import {loadAdditional} from "../common/startup.js";
@@ -18,6 +19,12 @@ export default (vue, zeroPage) => [
 
 			await loadAdditional();
 			zeroPage.cmd("siteClone", [config.templateAddress]);
+		}
+	},
+	{
+		path: "init-hub",
+		controller: () => {
+			vue.currentView = InitHub;
 		}
 	}
 ];
