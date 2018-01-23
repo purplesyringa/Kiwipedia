4be22f31d45e2a8317f99c7d712a67ce24615ed5
Ivanq <imachug@yandex.ru>
2018-01-23 19:15:13 +0300

    Add links to sidebar

--- main/src/vue_components/sidebar/sidebar.vue
+++ main/src/vue_components/sidebar/sidebar.vue
@@ -7,6 +7,18 @@
 			Wikipedia for ZeroNet with auto-import.<br>
 			Made with &#128155; by <a href="/Mail.ZeroNetwork.bit/?to=gitcenter">GitCenter</a> and community.
 		</div>
+
+		<div class="description">
+			<a href="?/" @click.prevent="$router.navigate('')">
+				Home
+			</a><br>
+			<a href="?/new-language" @click.prevent="$router.navigate('new-language')">
+				Don't see you language in list?
+			</a><br>
+			<a href="/13c2MpR9ztCzVm7r8cTfV9h9FFEEiQ6AKD">
+				Want to request a feature or report a bug?
+			</a>
+		</div>
 	</aside>
 </template>
 
