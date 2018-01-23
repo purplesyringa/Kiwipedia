4370adb47415f01b7df8ba44bbb5c4fc6e7182c4
Ivanq <imachug@yandex.ru>
2018-01-23 22:26:34 +0300

    Add init page

--- main/src/router_pages/init-hub/init-hub.vue
+++ main/src/router_pages/init-hub/init-hub.vue
@@ -4,11 +4,37 @@
 		<p>
 			Choose settings for your new hub.
 		</p>
+
+		<setting
+			name="Language"
+			description="English/Polish/etc."
+			ref="language"
+		/>
+		<setting
+			name="Subgroup (optional)"
+			description="ZeroNet development/America/etc."
+			ref="subgroup"
+		/>
+
+		<button @click="init">Init</button>
 	</div>
 </template>
 
+<style lang="sass" src="./init-hub.sass" scoped></style>
+
 <script type="text/javascript">
 	export default {
-		name: "init-hub"
+		name: "init-hub",
+		data() {
+			return {
+				language: null
+			};
+		},
+		methods: {
+			init() {
+				const language = this.$refs.language.value.trim();
+				const subgroup = this.$refs.subgroup.value.trim();
+			}
+		}
 	};
 </script>
