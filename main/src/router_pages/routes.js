da3f7502f0ac9c68ec8133f14845f4a81a521c69
Ivanq <imachug@yandex.ru>
2018-01-23 20:13:42 +0300

    Handle ?/new-language

--- main/src/router_pages/routes.js
+++ main/src/router_pages/routes.js
@@ -1,10 +1,17 @@
 import Home from "./home/home.vue";
 
-export default vue => [
+export default (vue, zeroPage) => [
 	{
 		path: "",
 		controller: () => {
 			vue.currentView = Home;
+		}
+	},
+	{
+		path: "new-language",
+		controller: async () => {
+			const siteInfo = await zeroPage.getSiteInfo();
+			zeroPage.cmd("siteClone", [siteInfo.address, "hub-template"]);
 		}
 	}
 ];
