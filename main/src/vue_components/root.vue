3bd891ca8a559a4f4dbe73757be6ad1f1d12fde0
Ivanq <imachug@yandex.ru>
2018-01-23 17:13:04 +0300

    Add simple header

--- main/src/vue_components/root.vue
+++ main/src/vue_components/root.vue
@@ -1,12 +1,21 @@
 <template>
-	<div class="current-view">
-		<component :is="$parent.currentView" ref="currentView"></component>
+	<div>
+		<sidebar />
+
+		<div class="current-view">
+			<component :is="$parent.currentView" ref="currentView"></component>
+		</div>
 	</div>
 </template>
 
 <script language="text/javascript">
+	import Sidebar from "./sidebar/sidebar.vue";
+
 	export default {
 		props: [],
-		name: "root"
+		name: "root",
+		components: {
+			sidebar: Sidebar
+		}
 	};
 </script>
---
+++ main/src/vue_components/sidebar/sidebar.vue
@@ -0,0 +1,14 @@
+<style lang="sass" src="./sidebar.sass" scoped></style>
+
+<template>
+	<aside>
+		<h1>Test</h1>
+	</aside>
+</template>
+
+<script language="text/javascript">
+	export default {
+		props: [],
+		name: "sidebar"
+	};
+</script>
