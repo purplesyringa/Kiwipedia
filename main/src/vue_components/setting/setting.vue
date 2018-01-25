c1e9e517a6f63792f2c4d3d0719ef7d33664828a
Ivanq <imachug@yandex.ru>
2018-01-25 10:49:21 +0300

    Add 'disabled' data

--- main/src/vue_components/setting/setting.vue
+++ main/src/vue_components/setting/setting.vue
@@ -4,10 +4,10 @@
 		<div class="description">{{description}}</div>
 
 		<div v-if="multiline">
-			<textarea v-model="value"></textarea>
+			<textarea v-model="value" :disabled="disabled"></textarea>
 		</div>
 		<div v-else>
-			<input type="text" v-model="value">
+			<input type="text" v-model="value" :disabled="disabled">
 		</div>
 	</div>
 </template>
@@ -20,7 +20,8 @@
 		props: ["name", "description", "multiline"],
 		data() {
 			return {
-				value: ""
+				value: "",
+				disabled: false
 			};
 		}
 	};
