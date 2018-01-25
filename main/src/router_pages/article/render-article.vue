9afa1da5d5cecbc6cee6258e850c1742212008fc
Ivanq <imachug@yandex.ru>
2018-01-25 18:21:03 +0300

    Render articles using marked

--- main/src/router_pages/article/render-article.vue
+++ main/src/router_pages/article/render-article.vue
@@ -1,8 +1,10 @@
 <template>
-	<p>{{text}}</p>
+	<div v-html="markdown"></div>
 </template>
 
 <script type="text/javascript">
+	import marked from "marked";
+
 	export default {
 		name: "markdown-article",
 		props: ["text"],
@@ -10,6 +12,11 @@
 			return {
 				text: ""
 			};
+		},
+		computed: {
+			markdown() {
+				return marked(this.text);
+			}
 		}
 	};
 </script>
