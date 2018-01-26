aa233233812b5895a4d80e36feb6a16cad7e9411
Ivanq <imachug@yandex.ru>
2018-01-26 16:09:28 +0300

    Add a link to 'Create a new article'

--- main/src/vue_components/hub-header/hub-header.vue
+++ main/src/vue_components/hub-header/hub-header.vue
@@ -1,10 +1,15 @@
 <template>
 	<div class="hub-header">
+		You are viewing hub
 		<b>{{hub.language}}</b>
 		<b v-if="hub.subgroup != ''">
 			<div class="bar" />
 			{{hub.subgroup}}
 		</b>
+
+		<br>
+
+		<a :href="`?/new-article/${hub.slug}`" @click.prevent="$router.navigate(`new-article/${hub.slug}`)">Create a new article</a>
 	</div>
 </template>
 
