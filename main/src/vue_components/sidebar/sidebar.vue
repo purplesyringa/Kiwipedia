2e2c0942d6032f3d693bd5d4368dd453a9127f26
Ivanq <imachug@yandex.ru>
2018-01-26 20:19:03 +0300

    Add logo

--- main/src/vue_components/sidebar/sidebar.vue
+++ main/src/vue_components/sidebar/sidebar.vue
@@ -2,7 +2,9 @@
 
 <template>
 	<aside>
-		<h1>ZeroWikipedia</h1>
+		<img src="./kiwi.svg" class="logo">
+		<h1 class="title">Zero<span>Wikipedia</span></h1>
+
 		<p>
 			Wikipedia for ZeroNet with auto-import.<br>
 			Made with &#128155; by <a href="/Mail.ZeroNetwork.bit/?to=gitcenter">GitCenter</a> and community.
