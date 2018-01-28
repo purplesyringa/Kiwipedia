448ebcfa0cefaf317575721e335936f9c0b4337d
Ivanq <imachug@yandex.ru>
2018-01-28 20:36:10 +0300

    Before Kiwipedia is released from beta, creating hub is limited to developers

--- main/src/router_pages/new-hub.vue
+++ main/src/router_pages/new-hub.vue
@@ -2,7 +2,8 @@
 	<div>
 		<h1>New Hub</h1>
 		<p>
-			Kiwipedia is creating your hub.
+			<!-- Kiwipedia is creating your hub. -->
+			Before Kiwipedia is released from beta, creating hub is limited to developers. If you want to add a hub, use <a href="/Me.ZeroNetwork.bit/?Profile/1SunAWK2VUT9GQK32MpwRfFPVgcBSJN9a/1Cy3ntkN2GN9MH6EaW6eHpi4YoRS2nK5Di/gitcenter@zeroid.bit">ZeroMe</a>.
 		</p>
 	</div>
 </template>
@@ -14,8 +15,10 @@
 	export default {
 		name: "new-hub",
 		async mounted() {
+			/*
 			await loadAdditional();
 			this.$zeroPage.cmd("siteClone", [config.templateAddress]);
+			*/
 		}
 	};
 </script>
