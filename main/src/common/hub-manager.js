776c24e69910f01d74aab136e6a9009e152cecc7
Ivanq <imachug@yandex.ru>
2018-01-30 18:49:21 +0300

    JOIN json twice to get cert_user_id

--- main/src/common/hub-manager.js
+++ main/src/common/hub-manager.js
@@ -4,10 +4,19 @@ export async function getHubList() {
 	return await zeroDB.query(`
 		SELECT
 			hubs.*,
-			json.cert_user_id AS runner
+			json_content.cert_user_id AS runner
 		FROM hubs
 
-		LEFT JOIN json USING (json_id)
+		LEFT JOIN json ON (
+			hubs.json_id = json.json_id
+			AND json.file_name = "data.json"
+		)
+
+		LEFT JOIN json AS json_content ON (
+			json.directory = json_content.directory
+			AND json.site = json_content.site
+			AND json_content.file_name = "content.json"
+		)
 
 		GROUP BY address
 	`);
