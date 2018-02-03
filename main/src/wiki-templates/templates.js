6c324116d64b553a398a96a91a4934cf4ba67b4c
Ivanq <imachug@yandex.ru>
2018-02-03 17:39:08 +0300

    Traverse through wiki-templates/ directory to get all templates

--- main/src/wiki-templates/templates.js
+++ main/src/wiki-templates/templates.js
@@ -1,44 +1,12 @@
-import CiteWeb from "./cite-web.js";
-import RefList from "./reflist.js";
-import AMBox from "./ambox.js";
-import UnexistingTemplate from "./unexisting-template.js";
-import InvalidTemplate from "./invalid-template.js";
-import ExternalMedia from "./external-media.js";
-import Ref from "./ref.js";
-import HashIf from "./#if.js";
-import If from "./if.js";
-import NumberOfArticles from "./numberofarticles.js";
-import MathTag from "./mathtag.js";
-import MathBrackets from "./math.js";
-import NoWrap from "./nowrap.js";
-import About from "./about.js";
-import HatNote from "./hatnote.js";
-import Main from "./main.js";
-import MVar from "./mvar.js";
-import SeeAlso from "./see-also.js";
+const context = require.context(".", true, /\.js$/);
 
 let Templates = {};
+for(let file of context.keys()) {
+	if(file == "./templates.js") {
+		continue;
+	}
 
-for(let template of [
-	CiteWeb,
-	RefList,
-	AMBox,
-	UnexistingTemplate,
-	InvalidTemplate,
-	ExternalMedia,
-	Ref,
-	HashIf,
-	If,
-	NumberOfArticles,
-	MathTag,
-	MathBrackets,
-	NoWrap,
-	About,
-	HatNote,
-	Main,
-	MVar,
-	SeeAlso
-]) {
+	let template = context(file).default;
 	Templates[template.name] = template;
 }
 
