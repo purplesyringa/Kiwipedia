5f013ccb162953c72e47c8e061e0f69f1686318f
Ivanq <imachug@yandex.ru>
2018-02-03 22:34:42 +0300

    Make <syntaxhighlight> nowiki, escape &, <, >, "

--- main/src/wiki-templates/syntaxhighlight.js
+++ main/src/wiki-templates/syntaxhighlight.js
@@ -1,6 +1,14 @@
 export default {
+	nowiki: true,
 	name: "<syntaxhighlight>",
 	async render(params, renderer) {
-		return `<pre>${params.original.replace(/\n/g, "<br>")}</pre>`;
+		return `<pre>${
+			params._
+				.replace(/&/g, "&amp;")
+				.replace(/</g, "&lt;")
+				.replace(/>/g, "&gt;")
+				.replace(/"/g, "&quot;")
+				.replace(/\n/g, "<br>")
+		}</pre>`;
 	}
 };
