7ced754c4b299ac825da015dde71366e425284ca
Ivanq <imachug@yandex.ru>
2018-02-05 21:57:35 +0300

    Rewrite <nowiki> prepare by using util

--- main/src/common/wiki-engine/plugins/nowiki.js
+++ main/src/common/wiki-engine/plugins/nowiki.js
@@ -2,51 +2,24 @@ import Templates from "../../../wiki-templates/templates.js";
 import htmlparser from "../htmlparser.js";
 import HTMLHandler from "../htmlhandler.js";
 import * as util from "../../../common/util.js";
+import * as pluginUtil from "./plugin-util.js";
 
 export function prepareNowiki(html) {
-	const handler = new HTMLHandler(`<div>\n${html}\n</div>`);
-	const parser = new htmlparser.Parser(handler);
-	parser.parseComplete(`<div>\n${html}\n</div>`);
-
-	const convertNowiki = elem => {
-		if(elem.type == "text") {
-			return elem.raw;
-		} else if(elem.type == "tag") {
-			let renderedInside = (elem.children || []).map(convertNowiki).join("");
-
-			return `<${elem.raw}>${renderedInside}</${elem.name}>`;
+	return pluginUtil.walkHtml(
+		html,
+		elem => {
+			return Templates[`<${elem.name}>`] && Templates[`<${elem.name}>`].nowiki;
+		},
+		(elem, renderedInside) => {
+			return `<kiwipedia-nowiki is="${elem.name}">` +
+				Object.keys(elem.attribs || {}).map(key => {
+					const value = elem.attribs[key];
+					return `<kiwipedia-param name="${key}">${value}</kiwipedia-param>`;
+				}).join("") +
+				`<kiwipedia-inside value="${util.base64encode(renderedInside)}" />` +
+			`</kiwipedia-nowiki>`;
 		}
-	};
-
-	const getInside = elem => {
-		const first = handler.tokens[elem.openTokenId];
-		const last = handler.tokens[elem.closeTokenId];
-
-		return `<div>\n${html}\n</div>`.substring(first.to + 1, last.from - 1);
-	};
-
-	const convert = elem => {
-		if(elem.type == "text") {
-			return elem.raw;
-		} else if(elem.type == "tag") {
-			if(Templates[`<${elem.name}>`] && Templates[`<${elem.name}>`].nowiki) {
-				const renderedInside = getInside(elem);
-
-				return `<kiwipedia-nowiki is="${elem.name}">` +
-					Object.keys(elem.attribs || {}).map(key => {
-						const value = elem.attribs[key];
-						return `<kiwipedia-param name="${key}">${value}</kiwipedia-param>`;
-					}).join("") +
-					`<kiwipedia-inside value="${util.base64encode(renderedInside)}" />` +
-				`</kiwipedia-nowiki>`;
-			}
-
-			let renderedInside = (elem.children || []).map(convert).join("");
-
-			return `<${elem.raw}>${renderedInside}</${elem.name}>`;
-		}
-	};
-	return convert(handler.dom[0]);
+	);
 };
 
 export async function renderNowiki(elem, convert, renderTemplate, renderData) {
