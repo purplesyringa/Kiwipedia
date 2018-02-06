f0a5eabf3f6290416033bd172992d5c1f5b60b5e
Ivanq <imachug@yandex.ru>
2018-02-06 22:09:53 +0300

    Move template parsing methods to plugin

--- main/src/common/wiki-engine/plugins/template.js
+++ main/src/common/wiki-engine/plugins/template.js
@@ -1,9 +1,64 @@
 import Templates from "../../../wiki-templates/templates.js";
-import {parseTemplate} from "../parser.js";
 import * as util from "../../util.js";
 
 const templateConstant = `MY_AWESOME_TEMPLATE_NUMBER_{{id}}_GOES_HERE_PLEASE_DONT_USE_THIS_CONSTANT_ANYWHERE_IN_ARTICLE`;
 const templateRegexp = /MY_AWESOME_TEMPLATE_NUMBER_(.+?)_GOES_HERE_PLEASE_DONT_USE_THIS_CONSTANT_ANYWHERE_IN_ARTICLE/g;
+
+function parseTemplateParams(params, trim=true) {
+	let index = 1;
+	let res = {};
+
+	params.split("|").forEach(param => {
+		if(trim) {
+			if(param.indexOf("=") == -1) {
+				res[index++] = param;
+			} else {
+				let name = param.substr(0, param.indexOf("=")).trim();
+				let value = param.substr(param.indexOf("=") + 1).trim();
+				res[name] = value;
+			}
+		} else {
+			res[index++] = param.trim();
+		}
+	});
+
+	return res;
+};
+
+function parseTemplate(template) {
+	if(template[0] == "#") {
+		let name = template.substr(0, template.indexOf(":"));
+		let params = template.substr(template.indexOf(":") + 1);
+
+		return {
+			name: name.trimLeft(),
+			params: parseTemplateParams(params, false)
+		};
+	}
+
+	let match = template.match(/^([^#<>\[\]\|\{\}]+?)\|([\s\S]*)$/);
+	if(match) {
+		return {
+			name: match[1].trim(),
+			params: parseTemplateParams(match[2])
+		};
+	}
+
+	match = template.match(/^([^#<>\[\]\|\{\}]+?)$/);
+	if(match) {
+		return {
+			name: match[1].trim(),
+			params: {}
+		};
+	}
+
+	return {
+		name: "invalid-template",
+		params: {
+			code: template
+		}
+	};
+};
 
 function replace(text, callback) {
 	return text
